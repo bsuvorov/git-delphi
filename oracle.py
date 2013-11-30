2dfe1fc6ded44165b42eafa1763f94aed7ded57d
Boris Suvorov <boris@box.com>
2013-11-29 16:54:14 -0800

    Oracle: ability to respond to GET requests with JSON reply of change descriptions.
    use searchterm=<term> as GET parameter to get response.

--- oracle.py
+++ oracle.py
@@ -1,1 +1,77 @@
-Oracle.py is a script that Delphi.html talks to get to get data about git history
+#!/usr/bin/python
+import os
+import MySQLdb
+import sys
+import json
+import cgi
+
+class changeDescription:
+	def __init__(self, message, author, date, impact, sha1):
+		self.message = message
+		self.author = author
+		self.date = date
+		self.impact = impact
+		self.sha1 = sha1
+
+
+def encode_changeDesc(obj):
+    if isinstance(obj, changeDescription):
+        return obj.__dict__
+    return obj
+
+def searchForTermWithLimit(term, limit):
+	db = MySQLdb.connect(host="localhost", 	# your host, usually localhost
+	                     user="root", 		# your username
+	                     passwd="root", 	# your password
+	                     db="adam")			# name of the data base
+
+	# you must create a Cursor object. It will let
+	#  you execute all the query you need
+	cur = db.cursor() 
+	# Use all the SQL you like
+				
+	mySQLQuery = """SELECT * FROM 
+						(SELECT hexsha, author, authored_date, message, MATCH (message) AGAINST ('%s' IN NATURAL LANGUAGE MODE) as score
+						FROM GITHISTORY
+						WHERE authored_date > '2012-01-01'
+						ORDER BY score DESC
+						LIMIT %s) a
+					WHERE score > 0""" % (term, limit)
+
+	cur.execute(mySQLQuery)
+	db.close()
+	
+	listOfChanges = []
+
+	# print all the first cell of all the rows
+	for row in cur.fetchall():
+		sha1 		= row[0]
+		author 		= row[1]
+		# convert date to the timestamp
+		timestamp	= row[2].strftime('%s')
+		message 	= row[3]
+		impact 		= row[4]
+
+		changeDesc = changeDescription(message, author, timestamp, impact, sha1)
+		listOfChanges.append(changeDesc)
+
+	return listOfChanges
+
+if __name__ == '__main__':
+
+	print 'Content-type: application/json\n\n'
+	
+
+	# the query string, which contains the raw GET data
+	# (For example, for http://example.com/myscript.py?a=b&c=d&e
+	# this is "a=b&c=d&e")
+	form = cgi.FieldStorage()
+	print form
+	searchTerm = "test"
+	if "searchterm" in form.keys():
+		searchTerm = form["searchterm"].value
+
+	print searchTerm
+	listOfChanges = searchForTermWithLimit(searchTerm, 5)
+
+	print json.dumps(listOfChanges, default=encode_changeDesc, indent=4, separators=(',', ': '))
